2d31a5c2d7332dbe606005c756f43a35f978f5df
kefah@Cocarz <kefah.bader@gmail.com>
2021-11-01 13:25:27 +0100

    Upgrading to NS8

--- demo/nativescript.config.ts
+++ demo/nativescript.config.ts
@@ -1,10 +1,10 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'org.nativescript.plugin.speechrecognition',
-  appResourcesPath: 'app/App_Resources',
+  id: "org.nativescript.plugin.speechrecognition",
+  appResourcesPath: "app/App_Resources",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none',
+    v8Flags: "--expose_gc",
+    markingMode: "none",
   }
 } as NativeScriptConfig;
